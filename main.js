09ccccbd2a62b37a0ae5bc5fed08287318f686ce
joechesney <33613119+joechesney@users.noreply.github.com>
2018-10-02 12:14:23 -0500

    created css file. Have not used it yet. Added form inputs for name, email address, and year, month, and date. year and month are being populated using js, but day dropdown is not yet working

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 
-// Using the MomentJs library to generate and format dynamic date
+// This uses the MomentJs library (node module) to generate and format dynamic date
 $("span#current-date").append(`${moment().format('dddd [, ] MMMM Do')}`);
+
+// ***MONTH DROPDOWN MENU***
+// populates month menu by looping over the array of month names
+// in string form from the moment method
+$.each(moment.months(), function(index, value){
+  $("#month-dropdown").append($("<option />").val(value).text(value));
+});
+
+// ***YEAR DROPDOWN MENU***
+// populates year menu from 1900 to current year
+for(let i = 1900; i <= moment().get('year'); i++){
+  $("#year-dropdown").append($("<option />").val(i).text(i));
+}
+
